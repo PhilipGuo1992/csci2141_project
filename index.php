f1d35cceeb8fdbd6fc3cbbd2e4eaebe7b1668bed
Alex Yorke <alhexyorke@users.noreply.github.com>
2018-03-09 02:19:16 +0000

    Initial commit for index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+$query = "select * from production;";
+if ($stmt = $connection->prepare($query)) {
+
+    /* execute statement */
+    $stmt->execute();
+
+    /* bind result variables */
+    $stmt->bind_result($name, $code);
+
+    /* fetch values */
+    while ($stmt->fetch()) {
+        printf ("%s (%s)\n", $name, $code);
+    }
+
+    /* close statement */
+    $stmt->close();
+}
+
+?>
