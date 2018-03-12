1a66869edbf8bf17e60f7aa4515091277b1f4349
Alex Yorke <alhexyorke@users.noreply.github.com>
2018-03-12 02:28:52 +0000

    Use printQueryToOptionList function

--- add_production_company.php
+++ add_production_company.php
@@ -1,7 +1,7 @@
 <?php
 include("dbconnect.php");
-$query = "select * from PRODUCTION";
 ?>
+
 <form action="add_production_company.php" method="GET">
 <h3>Enter name of a new production company.</h3>
 <input id="prod_name" placeholder="Name of new company." name="prod_name"></input>
@@ -11,31 +11,13 @@ $query = "select * from PRODUCTION";
 <?php
 
 if (isset($_GET["prod_name"])) {
-if ($stmt = $connection->prepare("insert into PRODUCTION values (NULL, '" . $_GET["prod_name"] . "');")) {
-$stmt->execute();
-echo "Company has been added! Check the bottom of this table.";
-}
-}
-?>
-
-<select name="prod_companies">
-<?php
-if ($stmt = $connection->prepare($query)) {
-
-    /* execute statement */
-    $stmt->execute();
-
-    /* bind result variables */
-    $stmt->bind_result($name, $code);
-
-    /* fetch values */
-    while ($stmt->fetch()) {
-        printf ("<option value='%s'>%s</option>", $name, $code);
+    if ($stmt = $connection->prepare("insert into PRODUCTION values (NULL, '" . $_GET["prod_name"] . "');")) {
+        $stmt->execute();
+        echo "Company has been added! Check the bottom of this table.";
     }
-
-    /* close statement */
-    $stmt->close();
-} else {echo "Error: " . $connection->error;}
+}
 ?>
+
+<select name="prod_companies"><?php printQueryToOptionList("select * from PRODUCTION"); ?></select>
 
 </table>
