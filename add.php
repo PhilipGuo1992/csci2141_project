50d5a2cbbd24c30b78c38e0e6a0959824828aa5c
Alex Yorke <alhexyorke@users.noreply.github.com>
2018-03-20 13:35:44 +0000

    Merge add functions into add.php

--- add.php
+++ add.php
@@ -2,7 +2,7 @@
 include("dbconnect.php");
 ?>
 
-<form action="add_production_company.php" method="GET">
+<form action="add.php" method="GET">
 <h3>Enter name of a new production company.</h3>
 <input id="prod_name" placeholder="Name of new company." name="prod_name"></input>
 <input type="submit"></input>
@@ -11,13 +11,13 @@ include("dbconnect.php");
 <?php
 
 if (isset($_GET["prod_name"])) {
-    if ($stmt = $connection->prepare("insert into PRODUCTION values (NULL, '" . $_GET["prod_name"] . "');")) {
+    if ($stmt = $connection->prepare("insert into production values (NULL, '" . $_GET["prod_name"] . "');")) {
         $stmt->execute();
         echo "Company has been added! Check the bottom of this table.";
     }
 }
 ?>
 
-<select name="prod_companies"><?php printQueryToOptionList("select * from PRODUCTION"); ?></select>
+<select name="prod_companies"><?php printQueryToOptionList("select * from production"); ?></select>
 
 </table>
