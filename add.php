3e95f5efe181ab10dd3c44ae011c62f2ea043174
Alex Yorke <alhexyorke@users.noreply.github.com>
2018-03-22 23:23:18 +0000

    Fix filename name

--- add.php
+++ add.php
@@ -67,7 +67,7 @@ tvshow
 	</tr>
 	<tr>
 		<td>Timeslot</td>
-		<td><a href="add_existing.php">Delete timeslot</a></td>
+		<td><a href="add_timeslot.php">Delete timeslot</a></td>
 		<td><a href="update_timeslot.php">Modify existing timeslot</a></td>
 		<td><a href="create_timeslot.php">Create New</a></td>
 	</tr>
