fd93640a01990b22f9e081718f8ead270ad5aa48
Unknown <philipguo1992@gmail.com>
2018-03-26 20:52:17 -0300

    update tvshow

---
+++ update_tvshow.php
@@ -0,0 +1,35 @@
+<?php
+include("dbconnect_local.php");
+
+$method = $_GET["method"];
+$newName = $_GET["newName"];
+$newYear = $_GET["newYear"];
+$showId = $_GET["showId"];
+?>
+
+<form action="update_tvshow.php" method="GET">
+<h3>Modify the Name</h3>
+<select name="showId"><?php printQueryToOptionList("select showId,concat(showName,releaseYear) from tvshow"); ?></select>
+<input id="newName" placeholder="Change the name to..." name="newName"></input>
+<input id="newYear" placeholder="Change Year to" name="newYear"></input>
+<input type="submit"></input>
+</form>
+
+
+<?php
+if (true) {
+    if ($stmt = $connection->prepare("update tvshow set showName = '" . $newName . "' where showId = " . $showId)) {
+        $stmt->execute();
+        echo "TV show has been updated! Please refresh the page to see your changes.<br />";
+    }
+	if ($stmt = $connection->prepare("update tvshow set releaseYear = '".$newYear."' where showId = " . $showId)) {
+        $stmt->execute();
+        echo "TV show has been updated! Please refresh the page to see your changes.<br />";
+    }
+} 
+
+	else if (empty($newCredit)) {
+	echo "The show name is empty. Please enter in a non-empty name.<br />";
+}
+
+?>
