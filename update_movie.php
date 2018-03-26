e693a36aca0b441067ebc6c1a2600ea76c6d971c
Navkaran0105 <navkarank@gmail.com>
2018-03-26 20:31:55 -0300

    update movie

--- update_movie.php
+++ update_movie.php
@@ -2,25 +2,36 @@
 include("dbconnect_local.php");
 
 $method = $_GET["method"];
-$newCredit = $_GET["newCredit"];
-$oldCredit = $_GET["oldCredit"];
+$newName = $_GET["newName"];
+$newYear = $_GET["newYear"]
+$oldName = $_GET["oldName"];
 ?>
 
-<form action="update_credits.php" method="GET">
+<form action="update_movie.php" method="GET">
 <h3>Modify the Credit Name</h3>
-<select name="oldCredit"><?php printQueryToOptionList("select creditId,fullName from credit"); ?></select>
-<input id="newCredit" placeholder="Change the name to..." name="newCredit"></input>
+<select name="movieId"><?php printQueryToOptionList("select movieId,concat(movieName,releaseYear) from movie"); ?></select>
+<input id="newName" placeholder="Change the name to..." name="newName"></input>
+<input id="newYear" placeholder="Change Year to" name="newYear"></input>
 <input type="submit"></input>
 </form>
 
 
 <?php
-if (isset($newCredit) && isset($oldCredit)) {
-    if ($stmt = $connection->prepare("update credit set fullName = '" . $newCredit . "' where creditId = " . $oldCredit)) {
+if (isset($newName) && isset($oldName)) {
+    if ($stmt = $connection->prepare("update credit set movieName = '" . $newName . "' and releaseYear = '.$newYear.' where movieId = " . $movieId)) {
         $stmt->execute();
-        echo "Genre has been updated! Please refresh the page to see your changes.<br />";
+        echo "Movie has been updated! Please refresh the page to see your changes.<br />";
     }
-} else if (empty($newCredit)) {
+} 
+
+
+else if (isset($newYear)) {
+	if ($stmt = $connection->prepare("update credit set releaseYear = '".$newYear."' where movieId = " . $movieId)) {
+        $stmt->execute();
+        echo "Movie has been updated! Please refresh the page to see your changes.<br />";
+    }
+}
+	else if (empty($newCredit)) {
 	echo "The Genre name is empty. Please enter in a non-empty name.<br />";
 }
 
