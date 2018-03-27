dcc0bff65045e9f296a8a14172f2d7882043708a
Navkaran0105 <navkarank@gmail.com>
2018-03-27 15:11:20 -0300

    update tvcontract

---
+++ update_tvcontract.php
@@ -0,0 +1,27 @@
+<?php
+include("dbconnect_local.php");
+
+$method = $_GET["method"];
+$newCredit = $_GET["newCredit"];
+$oldCredit = $_GET["oldCredit"];
+?>
+
+<form action="update_tvcontract.php" method="GET">
+<h3>Modify the Credit Name</h3>
+<select name="oldCredit"><?php printQueryToOptionList("select showId from tvcontract join tvshow"); ?></select>
+<select name="newCredit"><?php printQueryToOptionList("select creditId from tvcontract join credit"); ?></select>
+<input type="submit"></input>
+</form>
+
+
+<?php
+if (isset($newCredit) && isset($oldCredit)) { 
+    if ($stmt = $connection->prepare("update movie set creditId = '" . $newCredit . "' where creditId = " . $oldCredit)) {
+        $stmt->execute();
+        echo "The contact has been updated! Please refresh the page to see your changes.<br />";
+    }
+} else if (empty($newCredit)) {
+	echo "The contract is empty. Please enter in a non-empty name.<br />";
+}
+
+?>
