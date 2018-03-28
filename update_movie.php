422e432e6aea84ecebde0c59b701f5ce40656e5b
Navkaran0105 <navkarank@gmail.com>
2018-03-27 22:33:48 -0300

    update movie done

--- update_movie.php
+++ update_movie.php
@@ -2,27 +2,35 @@
 include("dbconnect_local.php");
 
 $method = $_GET["method"];
-$newCredit = $_GET["newCredit"];
-$oldCredit = $_GET["oldCredit"];
+$newName = $_GET["newName"];
+$newYear = $_GET["newYear"];
+$showId = $_GET["showId"];
 ?>
 
-<form action="update_movie.php" method="GET">
-<h3>Modify the Credit Name</h3>
-<select name="oldCredit"><?php printQueryToOptionList("select movieId,movieName from movie"); ?></select>
-<input id="newCredit" placeholder="Change the name to..." name="newCredit"></input>
+<form action="update_tvshow.php" method="GET">
+<h3>Modify the Name</h3>
+<select name="showId"><?php printQueryToOptionList("select movieId,concat(movieName,releaseYear) from movie"); ?></select>
+<input id="newName" placeholder="Change the name to..." name="newName"></input>
+<input id="newYear" placeholder="Change Year to" name="newYear"></input>
 <input type="submit"></input>
 <h5><a href="admin.php">Go To HomePage</a></h5>
 </form>
 
 
 <?php
-if (isset($newCredit) && isset($oldCredit)) { 
-    if ($stmt = $connection->prepare("update movie set movieName = '" . $newCredit . "' where movieIdId = " . $oldCredit)) {
+if (true) {
+    if ($stmt = $connection->prepare("update movie set movieName = '" . $newName . "' where movieId = " . $showId)) {
         $stmt->execute();
-        echo "Movie Name has been updated! Please refresh the page to see your changes.<br />";
+        echo "Movie has been updated! Please refresh the page to see your changes.<br />";
     }
-} else if (empty($newCredit)) {
-	echo "The Movie name is empty. Please enter in a non-empty name.<br />";
+	if ($stmt = $connection->prepare("update movie set releaseYear = '".$newYear."' where movieId = " . $showId)) {
+        $stmt->execute();
+        echo "Movie has been updated! Please refresh the page to see your changes.<br />";
+    }
+} 
+
+	else if (empty($newCredit)) {
+	echo "The movie name or Release Year is empty. Please enter in a non-empty name.<br />";
 }
 
 ?>
