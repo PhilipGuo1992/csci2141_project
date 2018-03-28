dc9367f887039e7b59642a81a03b666f88faa638
Navkaran0105 <navkarank@gmail.com>
2018-03-27 21:28:12 -0300

    new query

--- update_tvcontract.php
+++ update_tvcontract.php
@@ -8,8 +8,8 @@ $oldCredit = $_GET["oldCredit"];
 
 <form action="update_tvcontract.php" method="GET">
 <h3>Modify the TvContract</h3>
-<select name="oldCredit"><?php printMultiQueryToOptionList("select showId,showName from tvcontract from tvcontract join tvshow"); ?></select>
-<select name="newCredit"><?php printMultiQueryToOptionList("select creditId,fullName from tvcontract  from tvcontract join credit"); ?></select>
+<select name="newCredit"><?php printQueryToOptionList("select contractId,fullName from tvcontract join credit"); ?></select>
+<select name="oldCredit"><?php printQueryToOptionList("select showId,showName from tvshow"); ?></select>
 <input type="submit"></input>
 <h5><a href="admin.php">Go To HomePage</a></h5>
 </form>
@@ -19,7 +19,7 @@ $oldCredit = $_GET["oldCredit"];
 if (isset($newCredit) && isset($oldCredit)) { 
     if ($stmt = $connection->prepare("update movie set creditId = '" . $newCredit . "' where creditId = " . $oldCredit)) {
         $stmt->execute();
-        echo "The contact has been updated! Please refresh the page to see your changes.<br />";
+        echo "The contract has been updated! Please refresh the page to see your changes.<br />";
     }
 } else if (empty($newCredit)) {
 	echo "The contract is empty. Please enter in a non-empty name.<br />";
