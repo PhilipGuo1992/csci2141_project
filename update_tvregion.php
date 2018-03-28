36384a01ef8b683cd4ddb0cf4f6f71a3f27aebb5
Navkaran0105 <navkarank@gmail.com>
2018-03-28 11:31:53 -0300

    query fixed

--- update_tvregion.php
+++ update_tvregion.php
@@ -8,7 +8,8 @@ $oldCredit = $_GET["oldCredit"];
 
 <form action="update_tvregion.php" method="GET">
 <h3>Update Tvshow Coverage</h3>
-<select name="oldCredit"><?php printQueryToOptionList("select region_regionId,regionName from tvregion tvr join region r on r.regionId = tvr.region_regionId;"); ?></select>
+<select name="oldCredit"><?php printQueryToOptionList("select distinct(region_regionId),regionName from tvregion tvr 
+join region r on r.regionId = tvr.region_regionId"); ?></select>
 <select name="newCredit"><?php printQueryToOptionList("select showId,showName from tvshow"); ?></select>
 <input type="submit"></input>
 <h5><a href="admin.php">Go To HomePage</a></h5>
