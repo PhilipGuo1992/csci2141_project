9e78a25ae82e75e3f48de393b202147e06084dd7
Navkaran0105 <navkarank@gmail.com>
2018-03-28 00:26:56 -0300

    changed the column names in the update query

--- update_tvcontract.php
+++ update_tvcontract.php
@@ -8,8 +8,8 @@ $oldCredit = $_GET["oldCredit"];
 
 <form action="update_tvcontract.php" method="GET">
 <h3>Modify the TvContract</h3>
-<select name="newCredit"><?php printQueryToOptionList("select tvcontract.creditId,fullName from tvcontract join credit using(creditId) group by tvcontract.creditId"); ?></select>
-<select name="oldCredit"><?php printQueryToOptionList("select showId,showName from tvshow"); ?></select>
+<select name="oldCredit"><?php printQueryToOptionList("select tvcontract.creditId,fullName from tvcontract join credit using(creditId) group by tvcontract.creditId"); ?></select>
+<select name="newCredit"><?php printQueryToOptionList("select showId,showName from tvshow"); ?></select>
 <input type="submit"></input>
 <h5><a href="admin.php">Go To HomePage</a></h5>
 </form>
@@ -17,7 +17,7 @@ $oldCredit = $_GET["oldCredit"];
 
 <?php
 if (isset($newCredit) && isset($oldCredit)) { 
-    if ($stmt = $connection->prepare("update movie set creditId = '" . $newCredit . "' where creditId = " . $oldCredit)) {
+    if ($stmt = $connection->prepare("update tvcontract set showId = '" . $newCredit . "' where tvcontract.creditId = " . $oldCredit)) {
         $stmt->execute();
         echo "The contract has been updated! Please refresh the page to see your changes.<br />";
     }
