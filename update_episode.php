3f62ad330c95241e42c2e84a1f949d62bfee0ca8
Navkaran0105 <navkarank@gmail.com>
2018-03-27 21:55:03 -0300

    update episode updating runtime

--- update_episode.php
+++ update_episode.php
@@ -6,10 +6,10 @@ $newCredit = $_GET["newCredit"];
 $oldCredit = $_GET["oldCredit"];
 ?>
 
-<form action="update_movie.php" method="GET">
+<form action="update_episode.php" method="GET">
 <h3>Modify the Episode</h3>
 <select name="oldCredit"><?php printQueryToOptionList("select episodeId, CONCAT(showName, '(S', seasonNumber, ' E', episodeNumber, ')') from episode JOIN tvshow"); ?></select>
-<input id="newCredit" placeholder="Change the episodeNumber to..." name="newCredit"></input>
+<input id="newCredit" placeholder="Change the Runtime to..." name="newCredit"></input>
 <input type="submit"></input>
 <h5><a href="admin.php">Go To HomePage</a></h5>
 </form>
@@ -17,12 +17,12 @@ $oldCredit = $_GET["oldCredit"];
 
 <?php
 if (isset($newCredit) && isset($oldCredit)) { 
-    if ($stmt = $connection->prepare("update episode set episodeNumber = '" . $newCredit . "' where episodeId = " . $oldCredit)) {
+    if ($stmt = $connection->prepare("update episode set runtimeMinutes = '" . $newCredit . "' where episodeId = " . $oldCredit)) {
         $stmt->execute();
-        echo "Episode Number has been updated! Please refresh the page to see your changes.<br />";
+        echo "Runtime has been updated! Please refresh the page to see your changes.<br />";
     }
 } else if (empty($newCredit)) {
-	echo "The Episode Number  is empty. Please enter in a non-empty name.<br />";
+	echo "Runtime is empty. Please enter in a non-empty name.<br />";
 }
 
 ?>
