22573945d8a3e375394fab8c30cdf000c7101baf
Navkaran0105 <navkarank@gmail.com>
2018-03-27 22:35:24 -0300

    fixed double printing

--- update_movie.php
+++ update_movie.php
@@ -21,7 +21,6 @@ $showId = $_GET["showId"];
 if (true) {
     if ($stmt = $connection->prepare("update movie set movieName = '" . $newName . "' where movieId = " . $showId)) {
         $stmt->execute();
-        echo "Movie has been updated! Please refresh the page to see your changes.<br />";
     }
 	if ($stmt = $connection->prepare("update movie set releaseYear = '".$newYear."' where movieId = " . $showId)) {
         $stmt->execute();
